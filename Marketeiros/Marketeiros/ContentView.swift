11052e640161763e027ccb1ab6932af7c98a1f77
ingracristin <ingracristinaom@gmail.com>
2021-06-14 10:10:22 -0400

    primeiro

---
+++ Marketeiros/Marketeiros/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marketeiros
+//
+//  Created by Ingra Cristina on 14/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Marketeiros/Marketeiros/MarketeirosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketeirosApp.swift
+//  Marketeiros
+//
+//  Created by Ingra Cristina on 14/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MarketeirosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
