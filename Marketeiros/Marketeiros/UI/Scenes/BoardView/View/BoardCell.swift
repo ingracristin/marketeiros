c89158ee20d45c3b2ea43c283e2653f05288c6a2
guilherme <jguicf@gmail.com>
2021-06-17 10:56:10 -0400

    board view finished

--- Marketeiros/Marketeiros/UI/Scenes/BoardView/View/BoardCell.swift
+++ Marketeiros/Marketeiros/UI/Scenes/BoardView/View/BoardCell.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct BoardCell: View {
+    init(){
+        
+    }
+    
     var body: some View {
-        VStack{
-            HStack{
-                Text("Board 1")
+        
+        GeometryReader{ reader in
+            VStack(spacing: 0){
+                HStack{
+                    Text("Board 1")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    Spacer()
+                    Button(action: {}, label: {
+                        Text("...")
+                            .foregroundColor(.black)
+                            .font(.body)
+                            .fontWeight(.bold)
+                        
+                        
+                    })
+                }
+                HStack{
+                    Circle()
+                        .fill(Color.gray)
+                        .frame(width: reader.size.width * 0.15, height: reader.size.width * 0.15)
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/){
+                        Image(systemName: "plus.circle").resizable()
+                            .foregroundColor(Color(#colorLiteral(red: 0.6730545163, green: 0.658370316, blue: 0.6585103273, alpha: 1)))
+                    }.frame(width: reader.size.width * 0.15, height: reader.size.width * 0.15)
+                    Spacer()
+                }
                 
+                
+                Spacer()
+                    
             }
+            .padding()
+            .background(Color(#colorLiteral(red: 0.8469843268, green: 0.8471066356, blue: 0.8469573855, alpha: 1)))
         }
-        .background(Color(#colorLiteral(red: 0.8469843268, green: 0.8471066356, blue: 0.8469573855, alpha: 1)))
     }
 }
 
--- Marketeiros/Marketeiros/UI/Scenes/BoardView/View/BoardView.swift
+++ Marketeiros/Marketeiros/UI/Scenes/BoardView/View/BoardView.swift
@@ -9,34 +9,61 @@ import SwiftUI
 
 struct BoardView: View {
     var body: some View {
-        VStack{
-            HStack{
-                Text("Boards")
-                    .fontWeight(.bold)
-                    .font(.largeTitle)
-                Spacer()
-                Button(action: {}, label: {
-                    Text("Add Board")
-                        .foregroundColor(.white)
-                        .font(.caption)
-                        .fontWeight(.semibold)
-                        .padding(.init(top: 5, leading: 12, bottom: 5, trailing: 12))
-                        .background(RoundedRectangle(cornerRadius: 10))
-                })
-            }
+        GeometryReader{ reader in
+            VStack{
+                HStack{
+                    Text("Quadros")
+                        .fontWeight(.bold)
+                        .font(.largeTitle)
+                    Spacer()
+                    Button(action: {}, label: {
+                        Text("criar quadro")
+                            .foregroundColor(Color(#colorLiteral(red: 0.3960410953, green: 0.3961022794, blue: 0.3960276842, alpha: 1)))
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                            
+                    }).frame(width: reader.size.width * 0.2125, height: reader.size.height * 0.0295, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color(#colorLiteral(red: 0.7685593963, green: 0.7686710954, blue: 0.7685348988, alpha: 1)))
+                    .cornerRadius(12)
+                }
+                
+                ScrollView(.horizontal){
+                    LazyHStack(alignment: .top, spacing: 10, content: {
+                        ForEach(1...10, id: \.self) { count in
+                            BoardCell().frame(width: reader.size.width * 0.3671, height: reader.size.height * 0.2285, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .cornerRadius(14)
+                        }
+                    }).frame(height: reader.size.height * 0.2285)
+                }
+                HStack{
+                    Text("Cartões de Ideias")
+                        .fontWeight(.bold)
+                        .font(.largeTitle)
+                    Spacer()
+                    Button(action: {}, label: {
+                        Text("nova ideia")
+                            .foregroundColor(Color(#colorLiteral(red: 0.3960410953, green: 0.3961022794, blue: 0.3960276842, alpha: 1)))
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                            
+                    }).frame(width: reader.size.width * 0.2125, height: reader.size.height * 0.0295, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color(#colorLiteral(red: 0.7685593963, green: 0.7686710954, blue: 0.7685348988, alpha: 1)))
+                    .cornerRadius(12)
+                }
+                ScrollView(.horizontal){
+                    LazyVStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5, pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/, content: {
+                        ForEach(1...2, id: \.self) { count in
+                            IdeaCell()
+                                .frame(width: reader.size.width - 40, height: reader.size.height * 0.1228, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(Color(#colorLiteral(red: 0.8469843268, green: 0.8471066356, blue: 0.8469573855, alpha: 1)))
+                                .cornerRadius(14)
+                        }
+                    })
+                }
+                
+            }.padding(.horizontal,20)
             
-            ScrollView(.horizontal){
-                LazyHStack(alignment: .top, spacing: 10, content: {
-                    ForEach(1...10, id: \.self) { count in
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-                        })
-                    }
-                })
-            }
-        }.padding(.horizontal,20)
+        }
     }
-
 }
 
 struct BoardView_Previews: PreviewProvider {
