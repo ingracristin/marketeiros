29888b52d8047f1ca1bb4a9a3bd66232d2868898
guilherme <jguicf@gmail.com>
2021-06-29 11:12:27 -0400

    fix add board view

--- Marketeiros/Marketeiros/UI/Scenes/BoardView/View/NewBoardSheet.swift
+++ Marketeiros/Marketeiros/UI/Scenes/BoardView/View/NewBoardSheet.swift
@@ -12,7 +12,7 @@ struct NewBoardSheet: View {
     @State var selectdRole: String = "Editor"
     var body: some View {
         GeometryReader{ reader in
-            VStack(spacing:25){
+            VStack(alignment: .leading ,spacing:25){
                 Capsule()
                     .fill(Color.gray.opacity(0.5))
                     .frame(width: 50, height: 5)
@@ -40,7 +40,7 @@ struct NewBoardSheet: View {
                     HStack(spacing: 0){
                         Text("Nome do quadro").fontWeight(.semibold)
                             .font(.callout)
-                        Text("*").foregroundColor(.red)
+                        
                         Spacer()
                     }
                     TextField("", text: $boardName)
@@ -50,96 +50,47 @@ struct NewBoardSheet: View {
                 }
                 VStack(alignment:.leading, spacing: reader.size.height * 0.02){
                     
-                    Text("Adicione colaboradores").fontWeight(.semibold)
+                    Text("Bio do quadro").fontWeight(.semibold)
                         .font(.callout)
                     
-                    
-                    HStack{
-                        ZStack(alignment:.trailing){
-                            TextField("E-mail do convidado", text: $boardName)
-                                .padding()
-                                .frame(height:reader.size.height * 0.052)
-                                .background(Color(#colorLiteral(red: 0.7371894717, green: 0.7372970581, blue: 0.7371658683, alpha: 1)))
-                                .cornerRadius(8)
-                            
-                            Menu(content: {
-                                Button("Editor", action: editorSelect)
-                                Button("Leitor", action: readerSelect)
-                            }, label: {HStack(spacing: 5){
-                                Text(selectdRole)
-                                    .foregroundColor(Color(#colorLiteral(red: 0.3106196523, green: 0.3057384491, blue: 0.3057644367, alpha: 1)))
-                                    .font(.body)
-                                Image(systemName: "chevron.down")
-                                    .foregroundColor(Color(#colorLiteral(red: 0.3106196523, green: 0.3057384491, blue: 0.3057644367, alpha: 1)))
-                                
-                            }
-                            }).frame(height: reader.size.height * 0.052)
-                            .padding(.init(top: 0, leading: 0, bottom: 0, trailing: 10))
-                            
-                            
-                            
-                            
-                        }
-                        Button(action: {}, label: {
-                            Image(systemName: "paperplane").foregroundColor(.black)
-                            
-                        })
-                        .frame(width:reader.size.height * 0.052, height: reader.size.height * 0.052)
-                        .background(Color(#colorLiteral(red: 0.7685593963, green: 0.7686710954, blue: 0.7685348988, alpha: 1)))
-                        .cornerRadius(reader.size.height * 0.052)
-                        
-                        
-                    }
-                    
+                    TextEditor(text: $boardName)
+                        .colorMultiply(Color(#colorLiteral(red: 0.7371894717, green: 0.7372970581, blue: 0.7371658683, alpha: 1)))
+                        .cornerRadius(8)
+                        .frame(height: reader.size.height * 0.1317)
                     
                 }
                 
                 VStack(alignment:.leading, spacing: reader.size.height * 0.02){
                     
-                    Text("Convidar por link").fontWeight(.semibold)
+                    Text("Imagem do quadro").fontWeight(.semibold)
                         .font(.callout)
                     
                     
-                    HStack{
-                        ZStack(alignment:.trailing){
-                            TextField("https://da-br.zoom.us/859...", text: $boardName)
-                                .padding()
-                                .frame(height:reader.size.height * 0.052)
-                                .background(Color(#colorLiteral(red: 0.7371894717, green: 0.7372970581, blue: 0.7371658683, alpha: 1)))
-                                .cornerRadius(8)
-                            
-                            Menu(content: {
-                                Button("Editor", action: editorSelect)
-                                Button("Leitor", action: readerSelect)
-                            }, label: {HStack(spacing: 5){
-                                Text(selectdRole)
-                                    .foregroundColor(Color(#colorLiteral(red: 0.3106196523, green: 0.3057384491, blue: 0.3057644367, alpha: 1)))
-                                    .font(.body)
-                                Image(systemName: "chevron.down")
-                                    .foregroundColor(Color(#colorLiteral(red: 0.3106196523, green: 0.3057384491, blue: 0.3057644367, alpha: 1)))
-                                
-                            }
-                            }).frame(height: reader.size.height * 0.052)
-                            .padding(.init(top: 0, leading: 0, bottom: 0, trailing: 10))
-                            
-                            
-                            
-                            
+                    Button(action: {}, label: {
+                        ZStack(alignment: .center){
+                            Rectangle()
+                                .foregroundColor(Color(#colorLiteral(red: 0.7371894717, green: 0.7372970581, blue: 0.7371658683, alpha: 1)))
+                            VStack{
+                                Image(systemName: "camera")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: reader.size.width * 0.1413, height: reader.size.height * 0.0529)
+                                Text("Adicione uma capa")
+                            }.foregroundColor(Color(#colorLiteral(red: 0.3106196523, green: 0.3057384491, blue: 0.3057644367, alpha: 1)))
                         }
-                        Button(action: {}, label: {
-                            Image(systemName: "doc.on.doc").foregroundColor(.black)
-                            
-                        })
-                        .frame(width:reader.size.height * 0.052, height: reader.size.height * 0.052)
-                        .background(Color(#colorLiteral(red: 0.7685593963, green: 0.7686710954, blue: 0.7685348988, alpha: 1)))
-                        .cornerRadius(reader.size.height * 0.052)
                         
+                    })
+                    .frame(height:reader.size.height * 0.2512)
+                    .cornerRadius(8)
                         
-                    }
                     
                     
                 }
-                Spacer()
+                
+                
+                    
+                    
+                
                 
             }.background(BlurView(style: .systemMaterial))
             .padding(.horizontal,20)
