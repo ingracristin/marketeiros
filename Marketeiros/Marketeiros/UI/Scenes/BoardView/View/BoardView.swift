a646dbb1d32fdd66e84e6c726b3f8516f10ef57a
guilherme <jguicf@gmail.com>
2021-06-29 10:15:16 -0400

    main view finished

--- Marketeiros/Marketeiros/UI/Scenes/BoardView/View/BoardView.swift
+++ Marketeiros/Marketeiros/UI/Scenes/BoardView/View/BoardView.swift
@@ -10,24 +10,25 @@ import SwiftUI
 struct BoardView: View {
     var body: some View {
         GeometryReader{ reader in
-            VStack{
-                HStack{
-                    Text("Quadros")
-                        .fontWeight(.bold)
-                        .font(.largeTitle)
-                    Spacer()
-                    Button(action: {}, label: {
-                        Text("criar quadro")
-                            .foregroundColor(Color(#colorLiteral(red: 0.3960410953, green: 0.3961022794, blue: 0.3960276842, alpha: 1)))
-                            .font(.caption)
+            ScrollView(.vertical){
+                VStack(spacing: 20){
+                    HStack{
+                        Text("Seus Quadros")
                             .fontWeight(.semibold)
+                            .font(.title2)
+                        Spacer()
+                        Button(action: {}, label: {
+                            Text("criar quadro")
+                                .foregroundColor(Color(#colorLiteral(red: 0.3960410953, green: 0.3961022794, blue: 0.3960276842, alpha: 1)))
+                                .font(.caption)
+                                .fontWeight(.semibold)
                             
-                    }).frame(width: reader.size.width * 0.2125, height: reader.size.height * 0.0295, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .background(Color(#colorLiteral(red: 0.7685593963, green: 0.7686710954, blue: 0.7685348988, alpha: 1)))
-                    .cornerRadius(12)
-                }
-                
-                ScrollView(.horizontal){
+                        }).frame(width: reader.size.width * 0.2125, height: reader.size.height * 0.0295, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(Color(#colorLiteral(red: 0.7685593963, green: 0.7686710954, blue: 0.7685348988, alpha: 1)))
+                        .cornerRadius(12)
+                    }
+                    
+                    
                     LazyVStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5, pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/, content: {
                         ForEach(1...2, id: \.self) { count in
                             BoardCell()
@@ -35,32 +36,24 @@ struct BoardView: View {
                                 .cornerRadius(20)
                         }
                     })
+                    
+                    HStack{
+                        Text("Ideias")
+                            .fontWeight(.semibold)
+                            .font(.title2)
+
+                        Spacer()
+                        
+                    }
+                    
+                    
+                    IdeaCell()
+                        .frame(width: reader.size.width - 40, height: reader.size.height * 0.1280, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(Color(#colorLiteral(red: 0.9371728301, green: 0.9373074174, blue: 0.9371433854, alpha: 1)))
+                        .cornerRadius(14)
                 }
                 
-                HStack{
-                    Text("Cartões de Ideias")
-                        .fontWeight(.semibold)
-                        .font(.title)
-                    Spacer()
-                    Button(action: {}, label: {
-                        Text("nova ideia")
-                            .foregroundColor(Color(#colorLiteral(red: 0.3960410953, green: 0.3961022794, blue: 0.3960276842, alpha: 1)))
-                            .font(.caption)
-                            .fontWeight(.semibold)
-                            
-                    }).frame(width: reader.size.width * 0.2125, height: reader.size.height * 0.0295, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .background(Color(#colorLiteral(red: 0.7685593963, green: 0.7686710954, blue: 0.7685348988, alpha: 1)))
-                    .cornerRadius(12)
-                }
-                ScrollView(.vertical){
-                    LazyVStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5, pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/, content: {
-                        ForEach(1...10, id: \.self) { count in
-                            IdeaCell()
-                                .frame(width: reader.size.width - 40, height: reader.size.height * 0.1228, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(Color(#colorLiteral(red: 0.8469843268, green: 0.8471066356, blue: 0.8469573855, alpha: 1)))
-                                .cornerRadius(14)
-                        }
-                    })
-                }
+                
+                
                 
             }.padding(.horizontal,20)
             
--- Marketeiros/Marketeiros/UI/Scenes/BoardView/View/IdeaCell.swift
+++ Marketeiros/Marketeiros/UI/Scenes/BoardView/View/IdeaCell.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct IdeaCell: View {
-    @State var newIdea: String = ""
+    @State var newIdea: String = "Teve uma nova super ideia? Escreva aqui para não esquecer!"
     
     var body: some View {
-        VStack(alignment: .leading){
+        VStack(alignment: .leading, spacing: 5){
             HStack{
                 Button(action: {}, label: {
                     Image(systemName: "face.dashed").foregroundColor(Color(#colorLiteral(red: 0.3450980392, green: 0.2901960784, blue: 0.2901960784, alpha: 1)))
@@ -22,10 +22,15 @@ struct IdeaCell: View {
                     
                 })
             }
-            TextField("Teve uma nova super ideia? Escreva aqui para não esquecer!", text: $newIdea)
+            TextEditor(text: $newIdea)
+                .foregroundColor(Color(#colorLiteral(red: 0.5528908968, green: 0.5529734492, blue: 0.5528727174, alpha: 1)))
+                .colorMultiply(Color(#colorLiteral(red: 0.9371728301, green: 0.9373074174, blue: 0.9371433854, alpha: 1)))
             
-        }.padding(.horizontal,20)
-       
+            
+            
+        }.padding(.init(top: 12, leading: 20, bottom: 0, trailing: 20))
+        
+        
     }
 }
 
