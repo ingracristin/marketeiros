8dafbe0d754f1567a3bf7282950b5e2ed1fc2df2
ingracristin <ingracristinaom@gmail.com>
2021-07-01 11:53:24 -0400

    feat: moodboard

--- Marketeiros/Marketeiros/UI/Scenes/BoardView/View/InsideBoardView.swift
+++ Marketeiros/Marketeiros/UI/Scenes/BoardView/View/InsideBoardView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct InsideBoardView: View {
-    @State var selectedView = 0
+    @State var selectedView = 1
     @State var eita = false
     @ObservedObject var viewModel: InsideBoardViewModel
     
@@ -21,6 +21,14 @@ struct InsideBoardView: View {
         GridItem(.flexible(),spacing: 1),
         GridItem(.flexible(),spacing: 1)
     ]
+    let moodLayout = [
+        GridItem(.flexible(),spacing: 0.5),
+        GridItem(.flexible(),spacing: 0.5)
+    ]
+    let moodLayou = [
+        GridItem(.flexible(),spacing: 1)
+    ]
+    
     
     var body: some View {
         GeometryReader(){ reader in
@@ -36,13 +44,41 @@ struct InsideBoardView: View {
                 .pickerStyle(SegmentedPickerStyle())
                 
                 ScrollView(){
-                    LazyVGrid(columns: layout, spacing: 1){
-                        ForEach(1...18, id: \.self){ _ in
-                            Rectangle()
-                                .frame(height:reader.size.height * 0.15)
-                                .foregroundColor(Color(#colorLiteral(red: 0.9254091382, green: 0.9255421162, blue: 0.9253800511, alpha: 1)))
+                    if(selectedView == 0){
+                        LazyVGrid(columns: layout, spacing: 1){
+                            ForEach(1...18, id: \.self){ _ in
+                                Rectangle()
+                                    .frame(height:reader.size.height * 0.15)
+                                    .foregroundColor(Color(#colorLiteral(red: 0.9254091382, green: 0.9255421162, blue: 0.9253800511, alpha: 1)))
+                            }
                         }
+                    }else if(selectedView == 1){
+                        ForEach(1...9, id: \.self){ index in
+                            
+                                if(index % 2 == 0){
+                                    LazyVGrid(columns: moodLayou, spacing: 15){
+                                        Rectangle()
+                                            .foregroundColor(Color(#colorLiteral(red: 0.9254091382, green: 0.9255421162, blue: 0.9253800511, alpha: 1)))
+                                            .frame(width: 340, height: 195)
+                                            .cornerRadius(15)
+                                        
+                                    }
+                                }else{
+                                    LazyVGrid(columns: moodLayout, spacing: 10){
+                                        ForEach(1...2, id: \.self){ _ in
+                                            Rectangle()
+                                                .foregroundColor(Color(#colorLiteral(red: 0.9254091382, green: 0.9255421162, blue: 0.9253800511, alpha: 1)))
+                                                .frame(width: 150, height: 195)
+                                                .cornerRadius(15)
+                                        }
+                                    }
+                                    
+                                }
+                            
+                        }
+                        
                     }
+                    
                 }
             }.padding(.horizontal,20)
         }
@@ -87,6 +123,6 @@ struct InsideBoardView: View {
 
 struct InsideBoardView_Previews: PreviewProvider {
     static var previews: some View {
-        InsideBoardView(board: .init(uid: "", imagePath: "", title: "", description: "", instagramAccount: "", ownerUid: "", colaboratorsUids: [""], postsGridUid: "", ideasGridUid: "", moodGridUid: ""))
+        InsideBoardView(board: .init(uid: "ingracristin", imagePath: "", title: "", description: "", instagramAccount: "", ownerUid: "", colaboratorsUids: [""], postsGridUid: "", ideasGridUid: "", moodGridUid: ""))
     }
 }
---
+++ Marketeiros/Marketeiros/UI/Scenes/BoardView/View/MoodBoardUIView.swift
@@ -0,0 +1,20 @@
+//
+//  MoodBoardUIView.swift
+//  Marketeiros
+//
+//  Created by Ingra Cristina on 30/06/21.
+//
+
+import SwiftUI
+
+struct MoodBoardUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoodBoardUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoodBoardUIView()
+    }
+}
