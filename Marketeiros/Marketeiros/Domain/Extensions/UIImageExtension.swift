3353438a2ae3fb8bc0cc28c961474ead474f8568
guilherme <jguicf@gmail.com>
2021-07-01 12:12:18 -0400

    added average color feature

---
+++ Marketeiros/Marketeiros/Domain/Extensions/UIImageExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UIImageExtension.swift
+//  Marketeiros
+//
+//  Created by João Guilherme on 01/07/21.
+//
+
+import SwiftUI
+
+struct UIImageExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UIImageExtension_Previews: PreviewProvider {
+    static var previews: some View {
+        UIImageExtension()
+    }
+}
