7267e576b34ae54810016b96fa0370a6d5b5b39e
ingracristin <ingracristinaom@gmail.com>
2021-07-06 09:06:01 -0400

    feat: half modal

--- Marketeiros/Marketeiros/UI/Scenes/BoardView/View/MoodBoardView.swift
+++ Marketeiros/Marketeiros/UI/Scenes/BoardView/View/MoodBoardView.swift
@@ -8,21 +8,32 @@
 import SwiftUI
 
 struct MoodBoardView: View {
+    var flag: Bool = false
     var body: some View {
-        VStack{
-            Text("Voce ainda nao tem nada aqui mano")
-        }
-        .toolbar {
+        ZStack{
+            VStack{
+                Spacer()
+                MoodHalfModalView(offset: .constant(10))
+                    .frame(height: 120)
+            }
+            
+            VStack{
+                Text("Voce ainda nao tem nada aqui mano")
+            }
+            
+        }.toolbar {
             ToolbarItemGroup(placement: .bottomBar) {
                 Spacer()
                 Button(action: {
-                    MoodHalfModalView(offset: .constant(10))
+                    
+                    
                 }, label: {
                     Image(systemName: "plus").foregroundColor(.black)
                 })
                
             }
         }
+       
         
         
         
