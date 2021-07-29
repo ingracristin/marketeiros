52733c49b1d0efd62296cb88b453e3de39d462b2
guilherme <jguicf@gmail.com>
2021-07-29 09:53:09 -0400

    fixed board size

--- Marketeiros/Marketeiros/UI/Scenes/BoardView/View/BoardView.swift
+++ Marketeiros/Marketeiros/UI/Scenes/BoardView/View/BoardView.swift
@@ -53,7 +53,7 @@ struct BoardView: View {
                     }
                     .isHidden(true)
                 }.padding(.bottom,0)
-                    
+                
                 HStack {
                     Text("Home")
                         .navBarTitle()
@@ -62,69 +62,69 @@ struct BoardView: View {
                 .padding(.bottom,8)
                 .padding(.horizontal)
                 
-                GeometryReader { reader in
-                    ScrollView(.vertical, showsIndicators: false) {
-                        VStack(spacing: 20) {
-                            HStack{
-                                Text(NSLocalizedString("yourBoard", comment: ""))
-                                    .fontWeight(.semibold)
-                                    .font(.title2)
-                                    .foregroundColor(Color("NavBarTitle"))
-                                Spacer()
-                                AppButtonView(label: NSLocalizedString("createBtn", comment: "")) {
-                                    viewModel.toggleSheetView()
-                                }
-                            }.padding(.horizontal)
-                            
-                            LazyVStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5, pinnedViews: [], content: {
-                                ForEach(viewModel.boards, id: \.uid) { board in
-                                    NavigationLink(
-                                        destination: InsideBoardView(board: board),
-                                        label: {
-                                            BoardCell(board: board)
-                                                .frame(height: reader.size.height * 0.2955, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                                .background(Color(#colorLiteral(red: 0.8469843268, green: 0.8471066356, blue: 0.8469573855, alpha: 1)))
-                                                .cornerRadius(20)
-                                                .shadow(radius: 6, x: 2, y: 4)
-                                                .padding(.horizontal)
-                                                .padding(.vertical, 10)
-                                        })
-                                }
-                            })
-                            
-                            HStack{
-                                Text(NSLocalizedString("ideas", comment: ""))
-                                    .fontWeight(.semibold)
-                                    .font(.title2)
-                                    .foregroundColor(Color("NavBarTitle"))
-                                Spacer()
-                                AppButtonView(label: NSLocalizedString("createIdeaBtn", comment: "")) {
-                                    viewModel.toggleSheetView()
-                                }
-                            }.padding(.horizontal)
-                            
-                            IdeaCell()
-                                .background(Color(#colorLiteral(red: 0.9371728301, green: 0.9373074174, blue: 0.9371433854, alpha: 1)))
-                                .cornerRadius(14)
-                                .shadow(radius: 6)
-                                .padding(.horizontal)
-                          
-                        }
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack(spacing: 20) {
+                        HStack{
+                            Text(NSLocalizedString("yourBoard", comment: ""))
+                                .fontWeight(.semibold)
+                                .font(.title2)
+                                .foregroundColor(Color("NavBarTitle"))
+                            Spacer()
+                            AppButtonView(label: NSLocalizedString("createBtn", comment: "")) {
+                                viewModel.toggleSheetView()
+                            }
+                        }.padding(.horizontal)
+                        
+                        LazyVStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 5, pinnedViews: [], content: {
+                            ForEach(viewModel.boards, id: \.uid) { board in
+                                NavigationLink(
+                                    destination: InsideBoardView(board: board),
+                                    label: {
+                                        BoardCell(board: board)
+                                            .frame(height: UIScreen.main.bounds.size.height * 0.2955, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                            .background(Color(#colorLiteral(red: 0.8469843268, green: 0.8471066356, blue: 0.8469573855, alpha: 1)))
+                                            .cornerRadius(20)
+                                            .shadow(radius: 6, x: 2, y: 4)
+                                            .padding(.horizontal)
+                                            .padding(.vertical, 10)
+                                    })
+                            }
+                        })
+                        
+                        HStack{
+                            Text(NSLocalizedString("ideas", comment: ""))
+                                .fontWeight(.semibold)
+                                .font(.title2)
+                                .foregroundColor(Color("NavBarTitle"))
+                            Spacer()
+                            AppButtonView(label: NSLocalizedString("createIdeaBtn", comment: "")) {
+                                viewModel.toggleSheetView()
+                            }
+                        }.padding(.horizontal)
+                        
+                        IdeaCell()
+                            .background(Color(#colorLiteral(red: 0.9371728301, green: 0.9373074174, blue: 0.9371433854, alpha: 1)))
+                            .cornerRadius(14)
+                            .shadow(radius: 6)
+                            .padding(.horizontal)
+                        
                     }
                 }
-                .sheet(isPresented: viewModel.bindings.newBoardIsShowing, content: {
-                    NewBoardSheet(callback: { board in
-                        DispatchQueue.main.async {
-                            viewModel.add(board: board)
-                        }
-                    })
-                })
-                .navigationBarHidden(true)
-                
-                .onAppear {
-                    viewModel.getAllBoards()
-                }
             }
+            .sheet(isPresented: viewModel.bindings.newBoardIsShowing, content: {
+                NewBoardSheet(callback: { board in
+                    DispatchQueue.main.async {
+                        viewModel.add(board: board)
+                    }
+                })
+            })
+            .navigationBarHidden(true)
+            
+            .onAppear {
+                viewModel.getAllBoards()
+            }
+            
             //.padding(.horizontal,20)
         }
     }
