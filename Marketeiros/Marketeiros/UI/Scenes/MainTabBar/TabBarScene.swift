48316116757e19a97372b37ed2ed0b2c7af5fd50
guilherme <jguicf@gmail.com>
2021-07-29 10:29:43 -0400

    fixed tabbar selection icons

--- Marketeiros/Marketeiros/UI/Scenes/MainTabBar/TabBarScene.swift
+++ Marketeiros/Marketeiros/UI/Scenes/MainTabBar/TabBarScene.swift
@@ -8,29 +8,57 @@
 import SwiftUI
 
 struct TabBarScene: View {
+    @State var tabSelection = 0
+    @State var boardViewImage = "square.grid.2x2.fill"
+    @State var calendarViewImage = "calendar"
+    @State var profileViewImage = "person"
     var body: some View {
         NavigationView {
-            TabView {
+            TabView(selection: $tabSelection){
                 BoardView()
                 .tabItem {
-                    Label("Home", systemImage: "square.grid.2x2.fill")
+                    Label("Home", systemImage: boardViewImage)
                 }
+                .tag(0)
                 .accentColor(Color("tabBarItem"))
                 
                 CalendarPageView()
                     .tabItem {
-                        Label(NSLocalizedString("calendar", comment: ""), systemImage: "calendar")
+                        Label(NSLocalizedString("calendar", comment: ""), systemImage: calendarViewImage)
                     }
+                    .tag(1)
                     .accentColor(Color("tabBarItem"))
                     .navigationBarHidden(true)
                 
                 ProfileView()
                     .tabItem {
-                        Label(NSLocalizedString("profile", comment: ""), systemImage: "person.fill")
+                        Label(NSLocalizedString("profile", comment: ""), systemImage: profileViewImage)
                     }
+                    .tag(2)
                     .accentColor(Color("tabBarItem"))
                     .navigationBarHidden(true)
             }.accentColor(Color(UIColor.appLightBlue))
+            .onChange(of: tabSelection, perform: { value in
+                switch tabSelection{
+                case 0:
+                    boardViewImage = "square.grid.2x2.fill"
+                    calendarViewImage = "calendar"
+                    profileViewImage = "person"
+                case 1:
+                    boardViewImage = "square.grid.2x2"
+                    calendarViewImage = "calendar"
+                    profileViewImage = "person"
+                case 2:
+                    boardViewImage = "square.grid.2x2"
+                    calendarViewImage = "calendar"
+                    profileViewImage = "person.fill"
+                default:
+                    boardViewImage = "square.grid.2x2.fill"
+                    calendarViewImage = "calendar"
+                    profileViewImage = "person"
+                    
+                }
+            })
         }.accentColor(Color(UIColor.navBarItemsColor))
     }
 }
