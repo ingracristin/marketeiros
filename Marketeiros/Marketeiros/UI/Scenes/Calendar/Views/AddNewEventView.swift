d9d2572f2571c6dc6682850783e62c783a6109a1
guilherme <jguicf@gmail.com>
2021-08-03 10:54:43 -0400

    added new event screen

--- Marketeiros/Marketeiros/UI/Scenes/Calendar/Views/AddNewEventView.swift
+++ Marketeiros/Marketeiros/UI/Scenes/Calendar/Views/AddNewEventView.swift
@@ -15,38 +15,73 @@ struct AddNewEventView: View {
     @State var colaborators = ["Fulana", "Fulano", "Ciclano"]
     
     var body: some View {
-        VStack {
+        VStack(spacing:20){
             Capsule()
                 .fill(Color.gray.opacity(0.5))
                 .frame(width: 50, height: 5)
                 .padding(.top)
                 .padding(.bottom,5)
+            
+            HStack{
+                Button(action: {
+                    
+                }, label: {
+                    Text(NSLocalizedString("cancelBtn", comment: ""))
+                        .foregroundColor(Color(#colorLiteral(red: 0.8705882353, green: 0.3647058824, blue: 0.3647058824, alpha: 1)))
+                        .font(.body)
+                })
+                Spacer()
+                Text(NSLocalizedString("newEvent", comment: ""))
+                    .foregroundColor(Color("SheetHeaderColor"))
+                Spacer()
+                Button(action: {
+                    
+                }, label: {
+                    Text(NSLocalizedString("addEvent", comment: ""))
+                        .foregroundColor(Color("SheetButton"))
+                        .font(.body)
+                })
+            }
+            
             VStack(alignment: .leading) {
                 Text(NSLocalizedString("title", comment: ""))
                     .cellTitle()
                 TextField("", text: $title)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal,10)
+                    .frame(height: UIScreen.main.bounds.size.height * 0.0566)
+                    .foregroundColor(Color(#colorLiteral(red: 0.6117647059, green: 0.6039215686, blue: 0.6862745098, alpha: 1)))
+                    .background(Color("TextField2"))
+                    .cornerRadius(8)
             }
             
             HStack {
                 Text(NSLocalizedString("starts", comment: ""))
                     .cellTitle()
                 DatePicker("", selection: $date)
+                    .frame(height: UIScreen.main.bounds.size.height * 0.0566)
                     .datePickerStyle(CompactDatePickerStyle())
+                    //.background(Color("TextField2"))
             }
             
             HStack {
                 Text(NSLocalizedString("ends", comment: ""))
                     .cellTitle()
                 DatePicker("", selection: $date)
+                    .frame(height: UIScreen.main.bounds.size.height * 0.0566)
                     .datePickerStyle(CompactDatePickerStyle())
+                
+                    //.background(Color("TextField2"))
             }
             
             VStack(alignment: .leading) {
                 Text(NSLocalizedString("place", comment: ""))
                     .cellTitle()
                 TextField("", text: $title)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal,10)
+                    .frame(height: UIScreen.main.bounds.size.height * 0.0566)
+                    .foregroundColor(Color(#colorLiteral(red: 0.6117647059, green: 0.6039215686, blue: 0.6862745098, alpha: 1)))
+                    .background(Color("TextField2"))
+                    .cornerRadius(8)
             }
             
             HStack {
@@ -62,7 +97,7 @@ struct AddNewEventView: View {
                         ForEach(colaborators, id: \.self) { colaborator in
                             HStack() {
                                 Text(colaborator)
-                                    .foregroundColor(.white)
+                                    .foregroundColor(Color(#colorLiteral(red: 0.6117647059, green: 0.6039215686, blue: 0.6862745098, alpha: 1)))
                                     .padding(.vertical)
                                 Spacer()
                             }
@@ -72,49 +107,50 @@ struct AddNewEventView: View {
                 },
                 label: {
                     Text("Board 1")
-                        .foregroundColor(.white)
+                        .foregroundColor(Color(#colorLiteral(red: 0.6117647059, green: 0.6039215686, blue: 0.6862745098, alpha: 1)))
                         //.padding()
                 })
                 .padding()
-                .background(Color.red)
+                .background(Color("TextField2"))
                 .cornerRadius(8)
                 
-            HStack {
-                Text("Colaboradores")
-                    .cellTitle()
-                Spacer()
-            }
-            
-            DisclosureGroup(
-                isExpanded: $colaboratorsExpanded,
-                content: {
-                    VStack() {
-                        ForEach(colaborators, id: \.self) { colaborator in
-                            HStack() {
-                                Text(colaborator)
-                                    .foregroundColor(.white)
-                                    .padding(.vertical)
-                                Spacer()
-                            }
-                        }
-                    }
-                    .padding(.top)
-                },
-                label: {
-                    Text("Board 1")
-                        .foregroundColor(.white)
-                        //.padding()
-                })
-                .padding()
-                .background(Color.red)
-                .cornerRadius(8)
-                
-            Spacer()
+//            HStack {
+//                Text("Colaboradores")
+//                    .cellTitle()
+//                Spacer()
+//            }
+//
+//            DisclosureGroup(
+//                isExpanded: $colaboratorsExpanded,
+//                content: {
+//                    VStack() {
+//                        ForEach(colaborators, id: \.self) { colaborator in
+//                            HStack() {
+//                                Text(colaborator)
+//                                    .foregroundColor(.white)
+//                                    .padding(.vertical)
+//                                Spacer()
+//                            }
+//                        }
+//                    }
+//                    .padding(.top)
+//                },
+//                label: {
+//                    Text("Board 1")
+//                        .foregroundColor(.white)
+//                        //.padding()
+//                })
+//                .padding()
+//                .background(Color.red)
+//                .cornerRadius(8)
+//
+          Spacer()
         }
         //.background(BlurView(style: .systemMaterial))
         //.ignoresSafeArea(edges:.bottom)
-        .cornerRadius(30)
+        
         .padding()
+        
     }
 }
 
