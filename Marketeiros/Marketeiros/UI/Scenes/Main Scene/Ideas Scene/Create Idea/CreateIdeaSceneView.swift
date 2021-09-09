930fcb3e40908a6ebf8da02cf364975617549179
guilherme <jguicf@gmail.com>
2021-09-09 10:53:08 -0400

    swipe to delete almost finished

--- Marketeiros/Marketeiros/UI/Scenes/Main Scene/Ideas Scene/Create Idea/CreateIdeaSceneView.swift
+++ Marketeiros/Marketeiros/UI/Scenes/Main Scene/Ideas Scene/Create Idea/CreateIdeaSceneView.swift
@@ -10,18 +10,30 @@ import SwiftUI
 struct CreateIdeaSceneView: View {
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var viewModel: CreateIdeaSceneViewModel
+    @State var offset: CGFloat = 0
+    @State var isCollpsed: Bool = true
+    @State var pasteOffset: CGFloat = 0
+    @State var isSwipped: Bool = false
+    
+    func toggleBottomSheet() {
+        withAnimation {
+            offset = isCollpsed ? 350 : 0
+            isCollpsed.toggle()
+        }
+    }
     
     init(board: Board, pastes: [Paste], completion: ((Idea) -> ())?) {
         self.viewModel = CreateIdeaSceneViewModel(board: board, pastes: pastes, completion:completion)
     }
     
     var body: some View {
-        ZStack {
+        IdeasBottomSheetView(offset: $offset, isCollpsed: $isCollpsed) {
             VStack {
                 HStack {
                     Button(action: {
                         UIApplication.shared.endEditing()
-                        viewModel.togglePasteSheet()
+                        toggleBottomSheet()
+                        //viewModel.togglePasteSheet()
                     }, label: {
                         HStack {
                             Image(systemName: "slider.horizontal.3")
@@ -54,71 +66,126 @@ struct CreateIdeaSceneView: View {
                         }
                     }
             }.padding()
-
-            HalfModalView(isShown: viewModel.bindings.pasteSheetShowing, modalHeight: 350) {
-                ScrollView(.vertical, showsIndicators: false) {
-                    HStack {
-                        Button(action: {
-                            viewModel.setNonePaste()
-                            viewModel.togglePasteSheet()
-                        }) {
-                            Text(NSLocalizedString("cancel", comment: ""))
-                                .foregroundColor(.red)
-                        }
-                        Spacer()
-                        Text(NSLocalizedString("Categorizar   ", comment: "")) //trad
-                            .font(Font.sfProDisplaySemiBold(sized: 18))
-                            .foregroundColor(Color(UIColor.navBarTitleColor))
-                        Spacer()
-                        Button(action: {
-                            viewModel.togglePasteSheet()
-                        }) {
-                            Image(systemName: "plus")
-                                .resizable()
-                                .frame(width: 18, height: 18)
-                        }
-                    }.padding(.bottom)
-                    VStack {
-                        ForEach(viewModel.pastes, id:\.uid) { paste in
-                            if paste.uid != "none" {
-                                Button(action: {
-                                    viewModel.select(paste: paste)
-                                }) {
-                                    /*VStack {
-                                        HStack {
-                                            Image(systemName: "folder")
-                                            Text(paste.title)
-                                            Spacer()
-                                            if (viewModel.states.paste.uid == paste.uid) {
-                                                Image(systemName:"checkmark")
-                                            }
-                                        }
-                                        .foregroundColor(Color(UIColor.navBarTitleColor))
-                                        Divider()
-                                    }*/
-                                }.padding()
+        } sheet: {
+            ScrollView(.vertical, showsIndicators: false) {
+                HStack {
+                    Button(action: {
+                        //viewModel.togglePasteSheet()
+                        toggleBottomSheet()
+                    }) {
+                        Text(NSLocalizedString("cancel", comment: ""))
+                            .foregroundColor(.red)
+                    }
+                    Spacer()
+                    Text(NSLocalizedString("Categorizar   ", comment: "")) //trad
+                        .font(Font.sfProDisplaySemiBold(sized: 18))
+                        .foregroundColor(Color(UIColor.navBarTitleColor))
+                    Spacer()
+                    Button(action: {
+                        viewModel.togglePasteSheet()
+                    }) {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .frame(width: 18, height: 18)
+                    }
+                }.padding(.init(top: 20, leading: 0, bottom: 20, trailing: 0))
+                VStack(spacing: 0){
+                    ForEach(viewModel.pastes, id:\.uid) { paste in
+                        ZStack {
+                            HStack(spacing: 0){
+                                Spacer()
+                                Button(action: {}) {
+                                    Image(systemName: "square.and.pencil")
+                                        .foregroundColor(.white)
+                                }.frame(width: UIScreen.main.bounds.size.width * 0.144, height: UIScreen.main.bounds.size.height * 0.0566)
+                                .background(Color(#colorLiteral(red: 0.2733859122, green: 0.3903551698, blue: 0.9017891288, alpha: 1)))
+                                Button(action: {}) {
+                                    Image(systemName: "trash")
+                                        .foregroundColor(.white)
+                                }.frame(width: UIScreen.main.bounds.size.width * 0.144, height: UIScreen.main.bounds.size.height * 0.0566)
+                                .background(Color(#colorLiteral(red: 0.997802794, green: 0.2306014299, blue: 0.1866784096, alpha: 1)))
                             }
-                        }
+                            
+                            
+                            
+                            
+                            Button(action: {
+                                viewModel.select(paste: paste)
+                            }) {
+                                HStack {
+                                    
+                                    Text(paste.title)
+                                    Spacer()
+                                    if (viewModel.states.paste.uid == paste.uid) {
+                                        Image(systemName:"checkmark")
+                                    }
+                                }
+                                .frame(height: UIScreen.main.bounds.size.height * 0.0566)
+                                .background(Color("ModalSheetColor"))
+                                .foregroundColor(Color(UIColor.navBarTitleColor))
+                                
+                            }.offset(x: pasteOffset)
+                            .gesture(DragGesture().onChanged(onChanged(value:)).onEnded(onEnd(value:)))
+                            
+                        }.frame(height: UIScreen.main.bounds.size.height * 0.0566)
+                        Divider()
                     }
                 }
             }
-            .isHidden(!viewModel.bindings.pasteSheetShowing.wrappedValue)
-            .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
+            .padding(.horizontal,20)
+            .background(CornerShapeView(corners: [.topLeft,.topRight], radius: 30).foregroundColor(Color("ModalSheetColor")))
+            
+            
+            
         }
         .navigationBarTitle(NSLocalizedString("ideas", comment: ""),displayMode: .inline)
         .navigationBarItems(trailing:
-            HStack(spacing:20) {
-                if viewModel.states.okButtonShowing {
-                    Button {
-                        viewModel.saveIdea()
-                        self.presentationMode.wrappedValue.dismiss()
-                    } label: {
-                        Text("ok")
-                            .fontWeight(.semibold)
-                    }
-                }
-            }
+                                HStack(spacing:20) {
+                                    if viewModel.states.okButtonShowing {
+                                        Button {
+                                            viewModel.saveIdea()
+                                            self.presentationMode.wrappedValue.dismiss()
+                                        } label: {
+                                            Text("ok")
+                                                .fontWeight(.semibold)
+                                        }
+                                    }
+                                }
         )
+        
+        
+    }
+    func onChanged(value: DragGesture.Value){
+        
+        if value.translation.width < 0{
+            if(isSwipped){
+                pasteOffset = value.translation.width - 90
+            }else{
+                pasteOffset = value.translation.width
+            }
+            
+        }
+    }
+    
+    func onEnd(value: DragGesture.Value){
+        
+        withAnimation(.easeOut){
+            if(value.translation.width < 0){
+                if(-value.translation.width > UIScreen.main.bounds.width / 2){
+                    pasteOffset = -1000
+                }else if(-pasteOffset > 50){
+                    isSwipped = true
+                    pasteOffset = -90
+                }else{
+                    isSwipped = false
+                    pasteOffset = 0
+                }
+            }else{
+                isSwipped = false
+                pasteOffset = 0
+            }
+        }
+        
     }
 }
 
